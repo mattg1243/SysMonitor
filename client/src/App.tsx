0e9ff7e18d32381b2642a9d3383d9c0c319a8f59
Matt Gallucci <mattgallucci97@gmail.com>
2022-10-06 00:52:45 -0700

    added helpful useinterval hook, testing ipc comms

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,57 @@
-import React from 'react';
-import { Col, Divider, Row, Layout, Menu } from 'antd';
-import { Chart as ChartJS } from 'chart.js/auto';
-import { Chart } from 'react-chartjs-2';
-import data from './mockData';
+import React, { useEffect, useState, useRef } from 'react';
+import { Col, Row, Layout, Menu } from 'antd';
 import 'antd/dist/antd.css';
 
 const { Header, Content, Footer, Sider } = Layout;
+// a custom hook i found made by Dan Abramov
+function useInterval(callback, delay) {
+  const savedCallback = useRef<Function>();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
 
 function App() {
+  // states for data from main process
+  const [cpuPercentage, setCpuPercentage] = useState(0);
+  const [memPercentage, setMemPercentage] = useState(0);
+  const [batteryData, setBatteryData] = useState({});
+
+  const getCpuPercentage = async () => {
+    let cpuData = await window.api.getCpuPercentage();
+    setCpuPercentage(cpuData);
+    console.log('CPU data from Electron: ' + cpuData);
+  };
+
+  const getMemPercentage = async () => {
+    let memData = await window.api.getMemPercentage();
+    setMemPercentage(memData);
+  };
+
+  const getBatData = async () => {
+    let batData = await window.api.getBatPercentage();
+  };
+
+  const getAllData = async () => {
+    await getCpuPercentage();
+    await getMemPercentage();
+  };
+
+  useInterval(getAllData, 500);
+
   return (
     <div className="App">
       <Layout style={{ width: '100%', height: '100%' }}>
@@ -17,13 +61,15 @@ function App() {
         <Content style={{ padding: '0 50px', width: '100%', height: '100%' }}>
           <Row>
             <Col span={8} style={{ height: '100%', textAlign: 'center' }}>
-              CPU
+              <>CPU:</>
+              <p>%{parseFloat(cpuPercentage.toFixed(2))}</p>
             </Col>
             <Col span={8} style={{ height: '100%', textAlign: 'center' }}>
-              MEM
+              <>MEM Free:</>
+              <p>{parseFloat((memPercentage / 1000).toFixed(2))} GB</p>
             </Col>
             <Col span={8} style={{ height: '100%', textAlign: 'center' }}>
-              BAT
+              <>BAT: </>
             </Col>
           </Row>
         </Content>
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+declare global {
+  interface Window {
+    api: any;
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
