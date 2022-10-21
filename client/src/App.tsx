15d640bd0f40aafe3db68e6bbadac19160a9c5da
Matt Gallucci <mattgallucci97@gmail.com>
2022-10-21 13:55:51 -0700

    ipc logic working, cleaned up App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,41 +4,15 @@ import { Col, Row, Layout, Menu } from 'antd';
 import 'antd/dist/antd.css';
 import BatteryChart from './components/BatteryChart';
 import CpuChart from './components/CpuChart';
+import MemChart from './components/MemChart';
 const { Header, Content } = Layout;
 
 function App() {
-  // states for data from main process
-  const [cpuPercentage, setCpuPercentage] = useState(0);
-  const [memPercentage, setMemPercentage] = useState(0);
-  const [batteryData, setBatteryData] = useState({ percentage: 1.0 });
-
-  const getCpuPercentage = async () => {
-    // try {
-    //   let cpuData = await window.api.getCpuPercentage();
-    //   setCpuPercentage(cpuData);
-    //   console.log('CPU data from Electron: ' + cpuData);
-    // } catch (err) {
-    //   console.error(err.message);
-    // }
-  };
-
-  const getMemPercentage = async () => {
-    let memData = await window.api.getMemPercentage();
-    setMemPercentage(memData);
-  };
-
-  const getAllData = async () => {
-    await getCpuPercentage();
-    await getMemPercentage();
-  };
-
-  useInterval(getAllData, 2000);
-
   return (
     <div className="App">
       <Layout style={{ width: '100%', height: '100%' }}>
         <Header>
-          <Menu theme="dark" mode="horizontal" />
+          <Menu theme="dark" mode="horizontal" title="SysInfo" />
         </Header>
         <Content style={{ padding: '0 50px', width: '100%', height: '100%' }}>
           <Row>
@@ -48,7 +22,7 @@ function App() {
             </Col>
             <Col span={8} style={{ height: '100%', textAlign: 'center' }}>
               <>MEM Usage:</>
-              <p>{parseFloat((memPercentage / 1000).toFixed(2))} GB</p>
+              <MemChart />
             </Col>
             <Col span={8} style={{ height: '100%', textAlign: 'center' }}>
               <>BAT: </>
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import * as os from 'node-os-utils';
 import * as si from 'systeminformation';
 import * as path from 'path';
 import menuTemplate from './mainMenu';
-import { resolve } from 'path';
+import electronReload from 'electron-reload';
+electronReload(__dirname, {});
 // track env
 const env = process.env.NODE_ENV || 'development';
 
@@ -23,36 +24,42 @@ const createWindow = () => {
   win.webContents.openDevTools();
 };
 
-// app.on('ready', () => {
-//   createWindow();
-//   // // build menu from template
-//   // const mainMenu = Menu.buildFromTemplate(menuTemplate);
-//   // // insert menu
-//   // Menu.setApplicationMenu(mainMenu);
-// });
-
 // ipc logic
 ipcMain.handle('get-cpu-percentage', async (e, args) => {
-  const result = await new Promise(async (resolve, reject) => {
-    resolve(await os.cpu.free());
-  });
-  return result;
+  try {
+    const result = await new Promise(async (resolve, reject) => {
+      resolve(await os.cpu.free());
+    });
+    return result;
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 ipcMain.handle('get-mem-percentage', async (e, args) => {
-  const result = await new Promise(async (resolve, reject) => {
-    console.log('free mem: ' + (await os.mem.free()).freeMemMb / 1000 + ' GB');
-    resolve((await os.mem.free()).freeMemMb);
-  });
-  return result;
+  try {
+    const result = await new Promise(async (resolve, reject) => {
+      // console.log('free mem: ' + (await os.mem.free()).freeMemMb / 1000 + ' GB');
+      resolve((100 * (await os.mem.free()).freeMemMb) / (await os.mem.free()).totalMemMb);
+    });
+    return result;
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
-ipcMain.handle('get-bat-data', async (e, args) => {});
-// ipcMain.on('get-cpu-percentage', (e, args) => {
-//   os.cpuUsage(v => {
-//     e.returnValue(v);
-//   })
-// })
+ipcMain.handle('get-bat-data', async (e, args) => {
+  try {
+    const result = await new Promise(async (resolve, reject) => {
+      si.battery((v) => {
+        resolve(v.percent.toFixed(2));
+      });
+    });
+    return result;
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // start the app
 app.whenReady().then(() => {
@@ -76,11 +83,3 @@ app.on('window-all-closed', () => {
     app.quit();
   }
 });
-
-// dev runtime
-// if (env === 'development') {
-//   require('electron-reload')(__dirname, {
-//     electron: path.join(__dirname, '../node_modules', '.bin', 'electron'),
-//     hardResetMethod: 'exit',
-//   });
-// }
